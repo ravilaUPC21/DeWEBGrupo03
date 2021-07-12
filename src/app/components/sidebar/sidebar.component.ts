ed19a1ce88723846cdb8b976cc3aaf745b82e7c2
Eloy Avila <ravila2271@gmail.com>
2021-07-11 21:17:49 -0500

    Actualizar Menu

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -13,11 +13,7 @@ export const ROUTES: RouteInfo[] = [
   { path: '/change-password', title: 'Cambiar clave',  icon:'ni-circle-08 text-pink', class: '' },
   { path: '/register', title: 'Actualización',  icon:'ni-circle-08 text-pink', class: '' },
   { path: '/dashboard', title: 'Comprobantes de Pago',  icon: 'ni-tv-2 text-primary', class: '' },
-    // { path: '/icons', title: 'Icons',  icon:'ni-planet text-blue', class: '' },
-    // { path: '/maps', title: 'Maps',  icon:'ni-pin-3 text-orange', class: '' },
-  
-    // { path: '/tables', title: 'Tables',  icon:'ni-bullet-list-67 text-red', class: '' },
-    { path: '/login', title: 'Login',  icon:'ni-key-25 text-info', class: '' }
+  { path: '/login', title: 'Login',  icon:'ni-key-25 text-info', class: '' }
 
    
 ];
